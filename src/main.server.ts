78b03c126fad4cfc82140cf107ef0b17a4064cc7
Gonzalo Córdova Gonzales <gonzalocoop@gmail.com>
2025-11-29 19:56:16 -0500

    arreglos

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,19 @@
-import 'zone.js/node'; 
+// main.server.ts
+import 'zone.js/node';
 import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { config } from './app/app.config.server';
+import { config as originalConfig } from './app/app.config.server';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+
+// Creamos una copia de la configuración original y agregamos NoopAnimationsModule
+const config = {
+  ...originalConfig,
+  providers: [
+    ...(originalConfig.providers || []),
+    importProvidersFrom(NoopAnimationsModule), // <-- evita cargar @angular/animations/browser en SSR
+  ]
+};
 
 const bootstrap = (context: BootstrapContext) =>
     bootstrapApplication(App, config, context);
