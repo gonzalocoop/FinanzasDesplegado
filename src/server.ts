b20c473900b7105a7bf4ad7865c9b60c72c59e6f
Gonzalo Córdova Gonzales <gonzalocoop@gmail.com>
2025-11-29 20:22:50 -0500

    aaa

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const browserDistFolder = join(import.meta.dirname, '../browser');
 const app = express();
 const angularApp = new AngularNodeAppEngine();
 
-
+// Servir archivos estáticos
 app.use(
   express.static(browserDistFolder, {
     maxAge: '1y',
@@ -21,26 +21,28 @@ app.use(
   }),
 );
 
-
-app.use((req, res, next) => {
+// Manejar todas las rutas con SSR
+app.get('*', (req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => {
+      if (response) {
+        writeResponseToNodeResponse(response, res);
+      } else {
+        next();
+      }
+    })
     .catch(next);
 });
 
-
+// Iniciar servidor solo si es el módulo principal
 if (isMainModule(import.meta.url)) {
   const port = process.env['PORT'] || 4000;
   app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
-
+    if (error) throw error;
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
 
+// Exportar handler para Vercel
 export const reqHandler = createNodeRequestHandler(app);
